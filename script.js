c98ef0fc4dcbb1be657c5abee947a61b7be2bd55
Zaur Ibrahimov <zibrahimov95@gmail.com>
2025-01-27 21:50:49 +0200

    Remove utils.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { displayCountdown, displayStatusText } from "./src/utils.js";
-
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
@@ -226,6 +224,62 @@ window.addEventListener("load", function () {
   let enemyTimer = 0;
   let enemyInterval = 1000;
 
+  function displayCountdown(context, canvas) {
+    context.textAlign = "center";
+    context.font = "80px 'Press Start 2P', cursive";
+    context.shadowColor = "rgba(0, 0, 0, 0.8)";
+    context.shadowOffsetX = 3;
+    context.shadowOffsetY = 3;
+    context.shadowBlur = 5;
+
+    context.lineWidth = 7;
+    context.strokeStyle = "#000";
+    context.lineJoin = "round";
+    context.strokeText(
+      countdown > 0 ? countdown.toString() : "GO!",
+      canvas.width / 2,
+      canvas.height / 2
+    );
+    context.fillStyle = "white";
+    context.fillText(
+      countdown > 0 ? countdown.toString() : "GO!",
+      canvas.width / 2 + 2,
+      canvas.height / 2 + 2
+    );
+  }
+
+  function displayStatusText(context, canvas) {
+    context.textAlign = "left";
+    context.font = "30px 'Press Start 2P', cursive";
+    context.shadowColor = "rgba(0, 0, 0, 0.8)";
+    context.shadowOffsetX = 2;
+    context.shadowOffsetY = 2;
+    context.shadowBlur = 4;
+
+    context.lineWidth = 5;
+    context.strokeStyle = "#000";
+    context.lineJoin = "round";
+    context.strokeText("Score: " + score, 20, 50);
+    context.fillStyle = "white";
+    context.fillText("Score: " + score, 22, 52);
+
+    if (gameOver) {
+      context.textAlign = "center";
+      context.fillStyle = "black";
+      context.strokeText(
+        "GAME OVER, press Enter to restart!",
+        canvas.width / 2,
+        200
+      );
+      context.fillStyle = "white";
+      context.fillText(
+        "GAME OVER, press Enter to restart!",
+        canvas.width / 2 + 2,
+        202
+      );
+    }
+  }
+
   function animate(timeStamp) {
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
@@ -246,7 +300,7 @@ window.addEventListener("load", function () {
         gameStarted = true;
       }
 
-      displayCountdown(ctx, countdown, canvas);
+      displayCountdown(ctx, canvas);
     }
 
     if (gameStarted && !gameOver) {
@@ -255,7 +309,7 @@ window.addEventListener("load", function () {
       handleEnemies(deltaTime);
     }
 
-    displayStatusText(ctx, score, gameOver, canvas);
+    displayStatusText(ctx, canvas);
 
     if (!gameOver) requestAnimationFrame(animate);
   }
