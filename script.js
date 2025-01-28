7533af2d7acfa4b36c43232407ff5741943933d0
Zaur Ibrahimov <zibrahimov95@gmail.com>
2025-01-28 15:46:53 +0200

    Optimize for non-transparent canvas

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext("2d", { alpha: false });
 
   // Fixed canvas dimensions
   canvas.width = window.innerWidth;
